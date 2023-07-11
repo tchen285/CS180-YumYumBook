8e57db85b9d3be4aa1cee7bc902532ac05468355
tchen285 <91436200+tchen285@users.noreply.github.com>
2023-07-11 01:38:10 -0700

    The search function somewhat works, not perfect.

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom"
+import SearchOrder from "../features/order/SearchOrder";
 
 // Getting back to main menu function
 function Header() {
     return (
         <header>
             <Link to='/'>Yum Yum Book</Link>
+
+            <SearchOrder />
 
             <p>Tony</p>
         </header>
