62225cbe367c5478d1cc418a366d51badd1dcc7b
tchen285 <91436200+tchen285@users.noreply.github.com>
2023-07-11 12:17:29 -0700

    Styled texts differently

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,15 +1,16 @@
 import { Link } from "react-router-dom"
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 // Getting back to main menu function
 function Header() {
     return (
-        <header className="bg-yellow-500">
-            <Link to='/'>Yum Yum Book</Link>
+        <header className="bg-yellow-500 uppercase">
+            <Link to='/' className="tracking-widest">Yum Yum Book</Link>
 
             <SearchOrder />
 
-            <p>Tony</p>
+            <Username />
         </header>
     );
 }
