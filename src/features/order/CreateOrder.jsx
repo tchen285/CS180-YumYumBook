03dd904196d46861afd470d737223334f77fe771
tchen285 <91436200+tchen285@users.noreply.github.com>
2023-07-11 02:40:47 -0700

    Now if the custom enters a invalid phone number, the app will give a notice until the customer enters the valid phone number.

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/no-unescaped-entities */
 
 import { useState } from "react";
-import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../service/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
@@ -35,6 +35,10 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+
+  const formErrors = useActionData();
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
 
@@ -53,6 +57,7 @@ function CreateOrder() {
           <div>
             <input type="tel" name="phone" required />
           </div>
+          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
         <div>
@@ -75,25 +80,37 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button>Order now</button>
+          <button diabled={isSubmitting}>
+            {isSubmitting ? "Placing Order...." : "Order Now"}
+          </button>
         </div>
       </Form>
     </div>
   );
 }
 
-export async function action({request}) {
+export async function action({ request }) {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
 
-  const order ={
+  const order = {
     ...data,
     cart: JSON.parse(data.cart),
     priority: data.priority === "on",
   };
 
-  const newOrder = await createOrder(order);
+  const errors = {};
+  if (!isValidPhone(order.phone)) {
+    errors.phone =
+      "Please give us your correct phone number. We might need to contact you.";
+  }
 
+  if (Object.keys(errors).length > 0) {
+    return errors;
+  }
+
+  // Order after other things are correct!
+  const newOrder = await createOrder(order);
   return redirect(`/order/${newOrder.id}`);
 }
 
