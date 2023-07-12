f721a93ae6a281bf954de80c1b7aa16ef51e3e93
tchen285 <91436200+tchen285@users.noreply.github.com>
2023-07-11 19:06:54 -0700

    Changed font

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -5,7 +5,8 @@ import Username from "../features/user/Username";
 // Getting back to main menu function
 function Header() {
     return (
-        <header className="bg-yellow-500 uppercase px-4 py-3 border-b border-stone-200 sm:px-6 flex items-center justify-between">
+        <header className="bg-yellow-500 uppercase px-4 py-3 
+        border-b border-stone-200 sm:px-6 flex items-center justify-between font-pizza">
             <Link to='/' className="tracking-widest">Yum Yum Book</Link>
 
             <SearchOrder />
