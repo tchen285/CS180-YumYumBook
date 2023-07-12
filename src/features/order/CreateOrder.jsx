d5223947f9bdb91aeec9006192dc82c9c8cdd4d9
tchen285 <91436200+tchen285@users.noreply.github.com>
2023-07-11 21:32:22 -0700

    The Order Form looks much nicer, but still not perfect!

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -44,32 +44,32 @@ function CreateOrder() {
   const cart = fakeCart;
 
   return (
-    <div>
-      <h2>Ready to order? Let's go!</h2>
+    <div className="px-4 py-6">
+      <h2 className="text-xl font-semibold mb-8">Ready to order? Let's go!</h2>
 
       <Form method="POST">
-        <div>
-          <label>First Name</label>
-          <input className="input" type="text" name="customer" required />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">First Name</label>
+          <input className="input grow" type="text" name="customer" required />
         </div>
 
-        <div>
-          <label>Phone number</label>
-          <div>
-            <input className="input" type="tel" name="phone" required />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Phone number</label>
+          <div className="grow">
+            <input className="input w-full" type="tel" name="phone" required />
           </div>
-          {formErrors?.phone && <p>{formErrors.phone}</p>}
+          {formErrors?.phone && (<p className="mt-2 bg-red-100 text-xs text-red-700 p2 rounded-md">{formErrors.phone}</p>)}
         </div>
 
-        <div>
-          <label>Address</label>
-          <div>
-            <input className="input" 
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Address</label>
+          <div className="grow">
+            <input className="input w-full" 
             type="text" name="address" required />
           </div>
         </div>
 
-        <div>
+        <div className="mb-12 flex gap-5 items-center">
           <input
             className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring
              focus:ring-yellow-400 focus:ring-offset-2"
