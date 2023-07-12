c8ddbc786a1f605d1d51e8437d9c566b15d5edb5
tchen285 <91436200+tchen285@users.noreply.github.com>
2023-07-12 14:19:02 -0700

    Now, customers are able to input their names, and the names are showing all the different pages.

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react';
 import Button from "../../ui/Button";
+import { useDispatch } from 'react-redux';
+import { updateName } from './userSlice';
+import { useNavigate } from 'react-router-dom';
 
 function CreateUser() {
   const [username, setUsername] = useState('');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
+    if(!username) {
+      return;
+    }
+    dispatch(updateName(username));
+    navigate('/menu');
   }
 
   return (
@@ -14,7 +24,7 @@ function CreateUser() {
 
       <input
         type="text"
-        placeholder="Your full name"
+        placeholder="Your Name"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
         className='w-72 input mb-8'
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -5,7 +5,7 @@ import Username from "../features/user/Username";
 // Getting back to main menu function
 function Header() {
     return (
-        <header className="bg-yellow-500 uppercase px-4 py-3 
+        <header className="bg-blue-400 uppercase px-4 py-3 
         border-b border-stone-200 sm:px-6 flex items-center justify-between font-pizza">
             <Link to='/' className="tracking-widest font-extrabold">Yum Yum Book</Link>
 
